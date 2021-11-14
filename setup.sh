bb345c6640f9c9c75c7340ae9a8c90ce1b1388c4
pErfEcto <misha.yaro2004@gmail.com>
2021-11-14 13:04:28 +0100

    Setup file modified

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@ pip3 install -r requirements.txt
 echo "place here bot id "
 read var_bot_id
 echo $var_bot_id > bot_id
+echo "place here your telegram username "
+read var_username
+echo $var_username > creator
 ln -s /home/projects/watch_bot/watch_bot.service /etc/systemd/system/watch_bot.service
 systemctl enable watch_bot.service
 systemctl start watch_bot.service
